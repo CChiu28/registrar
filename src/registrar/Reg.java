1e1c2590611f7e9a4c27d47cdbae7a93b5037637
CChiu28 <ScorpiusDX@gmail.com>
2019-10-08 22:02:21 -0400

    tables

--- src/registrar/Reg.java
+++ src/registrar/Reg.java
@@ -1,11 +1,14 @@
 package registrar;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Reg {
 
 	public static void main(String[] args) {
 		Connection c = null;
+		Statement stmt = null;
 		try {
 			Class.forName("org.postgresql.Driver");
 			c = DriverManager
@@ -17,6 +20,29 @@ public class Reg {
 			System.exit(0);
 		}
 		System.out.println("Opened DB succcessfully");
+		
+		try {
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM students;");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String lname = rs.getString("lname");
+				String fname = rs.getString("fname");
+				System.out.println(id+"\t"+lname+"\t"+fname);
+			}
+			rs = stmt.executeQuery("SELECT * FROM class;");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String cname = rs.getString("class_name");
+				System.out.println(id+"\t"+cname);
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName()+" "+e.getMessage());
+			System.exit(0);
+		}
 	}
 
 }
--- table.sql
+++ table.sql
@@ -1,13 +1,14 @@
 
-CREATE TABLE IF NOT EXISTS students (
-    id INTEGER NOT NULL PRIMARY KEY,
-    lname TEXT NOT NULL,
-    fname TEXT NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS students (
+--     id INTEGER NOT NULL PRIMARY KEY,
+--     lname TEXT NOT NULL,
+--     fname TEXT NOT NULL
+-- );
 
-CREATE TABLE IF NOT EXISTS class (
-    id INTEGER NOT NULL PRIMARY KEY,
-    class_name TEXT NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS class (
+--     id INTEGER NOT NULL PRIMARY KEY,
+--     class_name TEXT NOT NULL
+-- );
 
-\COPY students FROM './MOCK_DATA.csv' WITH (FORMAT csv);
+-- \COPY students FROM './MOCK_DATA.csv' WITH (FORMAT csv);
+\COPY class FROM './MOCK_DATA2.csv' WITH (FORMAT csv);
