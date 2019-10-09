7b9e4f88f991409f9c555aaa3de95748bd05d1d9
cchiu28 <scorpiusdx@gmail.com>
2019-10-08 21:22:55 -0400

    init

---
+++ src/registrar/Reg.java
@@ -0,0 +1,22 @@
+package registrar;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Reg {
+
+	public static void main(String[] args) {
+		Connection c = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager
+					.getConnection("jdbc:postgresql://localhost:5432/registrar",
+				            "postgres", "admin");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName()+": "+e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened DB succcessfully");
+	}
+
+}
---
+++ table.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER NOT NULL PRIMARY KEY,
+    lname TEXT NOT NULL,
+    fname TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS class (
+    id INTEGER NOT NULL PRIMARY KEY,
+    class_name TEXT NOT NULL
+);
+
+\COPY students FROM './MOCK_DATA.csv' WITH (FORMAT csv);
