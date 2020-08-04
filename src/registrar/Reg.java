88e868d0cd87bbb6b8bf0c8dc51e0b0c34a90c59
CChiu28 <ScorpiusDX@gmail.com>
2020-08-04 01:40:59 -0400

    Editing studentOptions for more general usage later

--- src/registrar/Reg.java
+++ src/registrar/Reg.java
@@ -16,20 +16,21 @@ public class Reg {
 		CourseOptions cs = new CourseOptions();
 		
 		do {
-			System.out.println("Options:\n1. Add student\n2. Update student\n3. Search students\n4.Add course\n0. Quit\n8. Show courses\n9. List DB");
+			System.out.println("Options:\n1. Add student\n2. Update student\n3. Search students\n4. Delete student\n0. Quit\n8. Show courses\n9. List DB");
 			System.out.println("Select your option: ");
 			switch(scanner.next()) {
 			case "1":
-				student.addStudent();
+				addStudent(scanner, student);
 				break;
 			case "2":
-				student.updateStudent();
+				updateStudent(scanner, student);
 				break;
 			case "3":
 				student.findStudent();
 				break;
 			case "4":
-				
+				delStudent(student);
+				break;
 			case "0":
 				quit = true;
 				break;
@@ -48,26 +49,76 @@ public class Reg {
 		System.exit(0);
 	}
 	
-//	public static void addStudent(Scanner scanner, StudentOptions student) {
-//		int id;
-//		System.out.println("You have selected to add a new student");
-//		id = getIdInput(scanner);
-//		System.out.println(id);
-//		System.out.print("Enter last name: ");
-//		while (!scanner.hasNext("[a-zA-Z]+$")) {
-//			System.out.print("Enter a valid name: ");
-//			scanner.nextLine();
-//		}
-//		String lname = scanner.nextLine();
-//		System.out.println(lname);
-//		System.out.print("Enter first name:");
-//		while (!scanner.hasNext("[a-zA-Z]+")) {
-//			System.out.print("Enter a valid name: ");
-//			scanner.nextLine();
-//		}
-//		String fname = scanner.nextLine();
-//		System.out.println(fname);
-//		student.addStudent(id, lname, fname);
-//	}
-
+	public static void addStudent(Scanner scanner, StudentOptions student) {
+		int id;
+		System.out.println("You have selected to add a new student");
+		id = student.getIdInput();
+		System.out.println(id);
+//		scanner.nextLine();
+		System.out.print("Enter last name: ");
+//		scanner.nextLine();
+		while (!scanner.hasNext("[a-zA-Z]+$")) {
+			System.out.print("Enter a valid name: ");
+			scanner.nextLine();
+		}
+		String lname = scanner.nextLine();
+		System.out.println(lname);
+		System.out.print("Enter first name:");
+		while (!scanner.hasNext("[a-zA-Z]+")) {
+			System.out.print("Enter a valid name: ");
+			scanner.nextLine();
+		}
+		String fname = scanner.nextLine();
+		System.out.println(fname);
+		student.addStudent(id, fname, lname);
+	}
+	
+	public static void delStudent(StudentOptions stud) {
+		int id;
+		System.out.println("You have selected to delete a student");
+		id = stud.getIdInput();
+		System.out.println(id);
+		stud.delStudent(id);
+	}
+	public static void updateStudent(Scanner scan, StudentOptions stud) {
+		int findId = 0;
+		String lname = null, fname = null;
+		System.out.print("You have selected to update student information\n");
+		int id = stud.getIdInput();
+		ResultSet rs = stud.getStudentInfo(id);
+		try {
+			if (rs!=null)
+				while (rs.next()) {
+					System.out.println(rs.getString(1));
+					findId = Integer.parseInt(rs.getString(1));
+					lname = rs.getString(2);
+					fname = rs.getString(3);
+				}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (findId==0) {
+			System.out.println("Cannot find this ID");
+		} else {
+			System.out.println("We have found: ");
+			System.out.println(findId+"\t"+lname+"\t"+fname);
+			scan.nextLine();
+			System.out.print("Update last name: ");
+			while (!scan.hasNext("[a-zA-Z]+$")) {
+				System.out.print("Enter a valid name: ");
+				scan.nextLine();
+			}
+			String newLname = scan.nextLine();
+			System.out.println(newLname);
+			System.out.print("Update first name:");
+			while (!scan.hasNext("[a-zA-Z]+")) {
+				System.out.print("Enter a valid name: ");
+				scan.nextLine();
+			}
+			String newFname = scan.nextLine();
+			System.out.println(" Updating ID: "+findId+"\t"+newLname+", "+newFname);
+			stud.updateStudent(findId, newFname, newLname);
+		}
+	}
 }
--- src/registrar/StudentOptions.java
+++ src/registrar/StudentOptions.java
@@ -25,25 +25,25 @@ public class StudentOptions {
 		}
 	}
 	
-	public void addStudent() {
-		int id;
-		System.out.println("You have selected to add a new student");
-		id = getIdInput();
-		System.out.println(id);
-		System.out.print("Enter last name: ");
-		while (!scanner.hasNext("[a-zA-Z]+$")) {
-			System.out.print("Enter a valid name: ");
-			scanner.nextLine();
-		}
-		String lname = scanner.nextLine();
-		System.out.println(lname);
-		System.out.print("Enter first name:");
-		while (!scanner.hasNext("[a-zA-Z]+")) {
-			System.out.print("Enter a valid name: ");
-			scanner.nextLine();
-		}
-		String fname = scanner.nextLine();
-		System.out.println(fname);
+	public void addStudent(int id, String fname, String lname) {
+//		int id;
+//		System.out.println("You have selected to add a new student");
+//		id = getIdInput();
+//		System.out.println(id);
+//		System.out.print("Enter last name: ");
+//		while (!scanner.hasNext("[a-zA-Z]+$")) {
+//			System.out.print("Enter a valid name: ");
+//			scanner.nextLine();
+//		}
+//		String lname = scanner.nextLine();
+//		System.out.println(lname);
+//		System.out.print("Enter first name:");
+//		while (!scanner.hasNext("[a-zA-Z]+")) {
+//			System.out.print("Enter a valid name: ");
+//			scanner.nextLine();
+//		}
+//		String fname = scanner.nextLine();
+//		System.out.println(fname);
 		final String sql = "INSERT INTO students (id, lname, fname) "
 				+"VALUES (?, ?, ?);";
 		try (PreparedStatement pst = c.prepareStatement(sql)) {
@@ -63,6 +63,8 @@ public class StudentOptions {
 		try (PreparedStatement pst = c.prepareStatement(sql)){
 			pst.setInt(1, id);
 			pst.executeUpdate();
+			c.commit();
+			System.out.println("Student "+id+" is deleted\n");
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName()+": "+e.getMessage());
 		}
@@ -74,7 +76,7 @@ public class StudentOptions {
 		ResultSet rs = getStudentInfo(id);
 		String findId, lname, fname;
 		try {
-			if (!rs.next())
+			if (rs==null)
 				System.out.println("No student found with ID "+id);
 			else while (rs.next()) {
 				findId = rs.getString(1);
@@ -94,61 +96,62 @@ public class StudentOptions {
 		}
 	}
 	
-	public void updateStudent() {
-		int findId = 0;
-		String lname = null, fname = null;
-		System.out.print("You have selected to update student information");
-		int id = getIdInput();
-		ResultSet rs = getStudentInfo(id);
-		try {
-			if (rs!=null)
-				while (rs.next()) {
-					System.out.println(rs.getString(1));
-					findId = Integer.parseInt(rs.getString(1));
-					lname = rs.getString(2);
-					fname = rs.getString(3);
-				}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if (findId==0) {
-			System.out.println("Cannot find this ID");
-		} else {
-			System.out.println("We have found: ");
-			System.out.println(findId+"\t"+lname+"\t"+fname);
-			System.out.print("Update last name: ");
-			while (!scanner.hasNext("[a-zA-Z]+$")) {
-				System.out.print("Enter a valid name: ");
-				scanner.nextLine();
-			}
-			String newLname = scanner.nextLine();
-			System.out.println(newLname);
-			System.out.print("Update first name:");
-			while (!scanner.hasNext("[a-zA-Z]+")) {
-				System.out.print("Enter a valid name: ");
-				scanner.nextLine();
-			}
-			String newFname = scanner.nextLine();
-			System.out.println(" Updating ID: "+findId+"\t"+newLname+", "+newFname);
+	public void updateStudent(int findId, String newFname, String newLname) {
+//		int findId = 0;
+//		String lname = null, fname = null;
+//		System.out.print("You have selected to update student information");
+//		int id = 0;
+//		ResultSet rs = getStudentInfo(id);
+//		try {
+//			if (rs!=null)
+//				while (rs.next()) {
+//					System.out.println(rs.getString(1));
+//					findId = Integer.parseInt(rs.getString(1));
+//					lname = rs.getString(2);
+//					fname = rs.getString(3);
+//				}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		if (findId==0) {
+//			System.out.println("Cannot find this ID");
+//		} else {
+//			System.out.println("We have found: ");
+//			System.out.println(findId+"\t"+lname+"\t"+fname);
+//			System.out.print("Update last name: ");
+//			while (!scanner.hasNext("[a-zA-Z]+$")) {
+//				System.out.print("Enter a valid name: ");
+//				scanner.nextLine();
+//			}
+//			String newLname = scanner.nextLine();
+//			System.out.println(newLname);
+//			System.out.print("Update first name:");
+//			while (!scanner.hasNext("[a-zA-Z]+")) {
+//				System.out.print("Enter a valid name: ");
+//				scanner.nextLine();
+//			}
+//			String newFname = scanner.nextLine();
+//			System.out.println(" Updating ID: "+findId+"\t"+newLname+", "+newFname);
 			final String sql = "UPDATE students SET lname=?, fname=? WHERE id=?;";
 			try (PreparedStatement pst = c.prepareStatement(sql)) {
 				pst.setInt(3, findId);
 				pst.setString(1, newLname);
 				pst.setString(2, newFname);
 				int res = pst.executeUpdate();
+				DataSource.getInstance().printDataSource();
 				c.commit();
 				System.out.println(res+" records have been updated");
 			} catch (Exception e) {
 				System.err.println(e.getClass().getName()+": "+e.getMessage());
 			}
-		}
-		if(rs!=null)
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+//		}
+//		if(rs!=null)
+//			try {
+//				rs.close();
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
 	}
 	
 	public void listDB() {
@@ -167,14 +170,15 @@ public class StudentOptions {
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName()+" "+e.getMessage());
 			System.exit(0);
-		} finally {
-			if (rs != null) try { rs.close(); } catch (SQLException e) {e.printStackTrace();}
-			if (stmt != null) try { stmt.close(); } catch (SQLException e) {e.printStackTrace();}
-			if (c != null) try { c.close(); } catch (SQLException e) {e.printStackTrace();}
-		}
+		} 
+//		finally {
+//			if (rs != null) try { rs.close(); } catch (SQLException e) {e.printStackTrace();}
+//			if (stmt != null) try { stmt.close(); } catch (SQLException e) {e.printStackTrace();}
+//			if (c != null) try { c.close(); } catch (SQLException e) {e.printStackTrace();}
+//		}
 	}
 	
-	private int getIdInput() {
+	public int getIdInput() {
 		int id = 0;
 		System.out.print("Please enter an ID: ");
 		while (!scanner.hasNextInt()||(id=scanner.nextInt())<=0) {
@@ -185,7 +189,7 @@ public class StudentOptions {
 		return id;
 	}
 	
-	private ResultSet getStudentInfo(int id) {
+	public ResultSet getStudentInfo(int id) {
 		ResultSet rs = null;
 		final String sql = "SELECT * FROM students WHERE id=?;";
 		PreparedStatement pst = null;
